066348136a0bf95298407fad0a1eee3b459e267b
godfred2131 <maccarthy28@gmail.com>
2025-03-06 23:37:58 -0800

    New Update 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        string playAgain;
+        
+        do
+        {
+            int magicNumber = random.Next(1, 101);
+            int guess;
+            int attempts = 0;
+            
+            Console.WriteLine("Welcome to the Guess My Number game!");
+            
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+                
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {attempts} attempts.");
+                }
+            } while (guess != magicNumber);
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
+        
+        Console.WriteLine("Thanks for playing!");
     }
 }
